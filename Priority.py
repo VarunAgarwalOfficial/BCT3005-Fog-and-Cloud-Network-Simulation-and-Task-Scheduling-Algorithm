ff197b2003b05eb44657e00ac4a446903551ad49
Varun Agarwal <22291627+agarwalvarun1@users.noreply.github.com>
2024-01-21 20:05:17 +1100

    cleaned up old code

--- Priority.py
+++ Priority.py
@@ -1,187 +1,160 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 np.random.seed(2)
 
-def dist(node1, node2):
-    return ((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)**0.5
-
-
-
-# class Task:
-    # def __init__(self, priority, time , start_time):
-    #     self.priority = priority
-    #     self.time = time
-    #     self.done = False
-    #     self.start_time = start_time
-
+# Task class represents a computational task
 class Task:
-    def __init__(self, priority, time , start_time , deadline):
+    def __init__(self, priority, time, start_time):
         self.priority = priority
-        self.exec_time = time
+        self.time = time
         self.done = False
         self.start_time = start_time
-        self.deadline = deadline
-        self.cpu_cost = np.random.randint(0,10)
-        self.rom_cost = np.random.randint(0,10)
+        self.cpu_cost = np.random.randint(0, 10)
+        self.rom_cost = np.random.randint(0, 10)
 
     def __repr__(self):
         return f"Task({self.priority}, {self.time}, {self.done})"
 
+# Node class represents a computing node (FOG, USER, CLOUD)
 class Node:
-    def __init__(self, pos , type):
+    def __init__(self, pos, node_type):
         self.pos = pos
-        self.neighbors = []
-        self.type = type
-        self.tasks = []
+        self.neighbors = []  # Neighboring nodes
+        self.type = node_type
+        self.tasks = []  # Tasks assigned to the node
 
     def add_neighbor(self, node):
-        self.neighbors.append(node)
+        if node not in self.neighbors:
+            self.neighbors.append(node)
 
     def draw(self):
-        if self.type == "FOG":
-            if len(self.tasks) > 0:
-                plt.plot(self.pos[0], self.pos[1], 'ro' , markeredgecolor='r' , markersize=10)
-            else:
-                plt.plot(self.pos[0], self.pos[1], 'ro' , markerfacecolor='none', markeredgecolor='r' , markersize=10)
-        elif self.type == "CLOUD":
-            plt.plot(self.pos[0], self.pos[1], 'bo' , markerfacecolor='b', markeredgecolor='b' , markersize=10)
-        else:
-            if len(self.tasks) > 0:
-                plt.plot(self.pos[0], self.pos[1], 'go' , markeredgecolor='g' , markersize=10)
-            else:
-                plt.plot(self.pos[0], self.pos[1], 'go' , markerfacecolor='none', markeredgecolor='g' , markersize=10)
-        for n in self.neighbors:
-            plt.plot([self.pos[0], n.pos[0]], [self.pos[1], n.pos[1]], 'black')
+        # Plotting nodes based on type and tasks assigned
+        marker_color = 'r' if self.type == "FOG" else 'b' if self.type == "CLOUD" else 'g'
+        marker_face_color = 'none' if (self.type == "FOG" or self.type == "USER") and len(self.tasks) == 0 else marker_color
+        plt.plot(self.pos[0], self.pos[1], f'{marker_color}o', markerfacecolor=marker_face_color,
+                 markeredgecolor=marker_color, markersize=10)
 
+        # Drawing edges between neighboring nodes
+        for neighbor in self.neighbors:
+            plt.plot([self.pos[0], neighbor.pos[0]], [self.pos[1], neighbor.pos[1]], 'black')
 
-
-
-
+# Graph class represents the entire computing graph
 class Graph:
     def __init__(self):
-        self.nodes = []
-        self.completed = []
-        self.cloud_used = 0
-        self.fog_used = 0
+        self.nodes = []  # List of nodes in the graph
+        self.completed = []  # Completed tasks
+        self.cloud_used = 0  # Total cloud processing time
+        self.fog_used = 0  # Total fog processing time
 
     def add_node(self, node):
         self.nodes.append(node)
 
-
     def connect(self):
-        for i in range(len(self.nodes)):
-            if (self.nodes[i].type == "FOG"):
-                self.nodes[i].add_neighbor(self.nodes[-1])
-                self.nodes[-1].add_neighbor(self.nodes[i])
+        for i, node_i in enumerate(self.nodes):
+            if node_i.type == "FOG":
+                self.connect_fog(node_i)
+            elif node_i.type == "USER":
+                self.connect_user(node_i)
 
-            if (self.nodes[i].type == "USER"):
-                min_dist = 100000
-                min_node = None
-                for j in range(len(self.nodes)):
-                    if (self.nodes[j].type == "FOG"):
-                        
-                        distance = dist(self.nodes[i].pos, self.nodes[j].pos)
-                        if (distance < min_dist):
-                            min_dist = distance
-                            min_node = self.nodes[j]
-                self.nodes[i].add_neighbor(min_node)
+    def connect_fog(self, fog_node):
+        # Connects a fog node to the last node in the graph (assumed to be CLOUD)
+        fog_node.add_neighbor(self.nodes[-1])
+        self.nodes[-1].add_neighbor(fog_node)
+
+    def connect_user(self, user_node):
+        # Connects a user node to the nearest fog node
+        min_dist = float('inf')
+        min_fog_node = None
+        for fog_node in filter(lambda x: x.type == "FOG", self.nodes):
+            distance = np.linalg.norm(np.array(user_node.pos) - np.array(fog_node.pos))
+            if distance < min_dist:
+                min_dist = distance
+                min_fog_node = fog_node
+        user_node.add_neighbor(min_fog_node)
 
     def draw(self):
         plt.clf()
         for node in self.nodes:
             node.draw()
-        plt.pause(0.0000000001)
+        plt.pause(1)
 
     def generate_task(self, time):
-        for node in self.nodes:
-            if (node.type == "USER"):
-                # 10 % probability of generating a task                                                                  
-                if np.random.rand() < 0.2:
-                    node.tasks.append(Task(np.random.randint(1, 10), np.random.randint(1, 10) , time))
+        # Generates tasks for USER nodes with a 20% probability
+        for user_node in filter(lambda x: x.type == "USER", self.nodes):
+            if np.random.rand() < 0.2:
+                user_node.tasks.append(Task(np.random.randint(1, 10), np.random.randint(1, 10), time))
 
     def transfer_task(self):
-        for node in self.nodes:
-            if node.type == "USER" and len(node.tasks) > 0:
-                task = node.tasks.pop(0)
-                node.neighbors[0].tasks.append(task)
+        # Transfers tasks from USER nodes to their neighboring FOG nodes
+        for user_node in filter(lambda x: x.type == "USER" and len(x.tasks) > 0, self.nodes):
+            task = user_node.tasks.pop(0)
+            user_node.neighbors[0].tasks.append(task)
 
-    def fog(self , time):
-        for node in self.nodes:
-            if node.type == "FOG" and len(node.tasks) > 0:
-                for task in node.tasks:
-                    if task.priority < 5:
-                        task.start_time -= 3
-                        
-                        self.nodes[-1].tasks.append(task)
-                        node.tasks.remove(task)
-                    
-                node.tasks.sort(key=lambda x: x.priority, reverse=True)
-                if len(node.tasks) > 0:
-                    node.tasks[0].time -= 3
-                    self.fog_used += 2
-                    if node.tasks[0].time <= 0:
-                        node.tasks[0].done = True
-                        node.tasks[0].end_time = time
-                        self.completed.append(node.tasks[0])
-                        node.tasks.remove(node.tasks[0])
-                for i in range( 1, len(node.tasks)):
-                    self.nodes[-1].tasks.append(node.tasks.pop(-1))
-                    
-
-        
-    def cloud(self , time):
-        for node in self.nodes:
-            if node.type == "CLOUD" and len(node.tasks) > 0:
-                node.tasks.sort(key=lambda x: x.priority, reverse=True)
-                # print(len(node.tasks))
-                i = 0
-                for task in node.tasks:
-                    self.cloud_used += task.time
-                    task.done = True
-                    task.end_time = time + i
-                    self.completed.append(task)
+    def process_node_tasks(self, node, time):
+        # Processes tasks for a given node
+        if len(node.tasks) > 0:
+            for task in node.tasks:
+                if task.priority < 5:
                     task.start_time -= 3
+                    self.nodes[-1].tasks.append(task)
                     node.tasks.remove(task)
-                    i+= task.time // 4
 
+            node.tasks.sort(key=lambda x: x.priority, reverse=True)
+            if len(node.tasks) > 0:
+                node.tasks[0].time -= 3
+                self.fog_used += 2
+                if node.tasks[0].time <= 0:
+                    node.tasks[0].done = True
+                    node.tasks[0].end_time = time
+                    self.completed.append(node.tasks[0])
+                    node.tasks.remove(node.tasks[0])
 
-'''
-CONSTANTS
-'''
+            for i in range(1, len(node.tasks)):
+                self.nodes[-1].tasks.append(node.tasks.pop(-1))
+
+    def fog(self, time):
+        # Processes tasks for FOG nodes
+        for fog_node in filter(lambda x: x.type == "FOG" and len(x.tasks) > 0, self.nodes):
+            self.process_node_tasks(fog_node, time)
+
+    def cloud(self, time):
+        # Processes tasks for CLOUD node
+        for cloud_node in filter(lambda x: x.type == "CLOUD" and len(x.tasks) > 0, self.nodes):
+            cloud_node.tasks.sort(key=lambda x: x.priority, reverse=True)
+            i = 0
+            for task in cloud_node.tasks:
+                self.cloud_used += task.time
+                task.done = True
+                task.end_time = time + i
+                self.completed.append(task)
+                task.start_time -= 3
+                cloud_node.tasks.remove(task)
+                i += task.time // 4
+
 
 FOG = 5
 USERS = 50
+
 def main():
     graph = Graph()
-    for i in range(FOG):
-        graph.add_node(Node([np.random.randint(low = -100 , high = 100) , np.random.randint(low = -100 , high = 100)], "FOG"))
+    for _ in range(FOG):
+        graph.add_node(Node([np.random.randint(low=-100, high=100), np.random.randint(low=-100, high=100)], "FOG"))
 
-    for i in range(USERS):
-        graph.add_node(Node([np.random.randint(low = -300 , high = 300) , np.random.randint(low = -300 , high = 300)], "USER"))
-    
-    graph.add_node(Node([0,0] , "CLOUD"))
+    for _ in range(USERS):
+        graph.add_node(Node([np.random.randint(low=-300, high=300), np.random.randint(low=-300, high=300)], "USER"))
 
+    graph.add_node(Node([0, 0], "CLOUD"))
 
     graph.connect()
-    graph.generate_task(0)
     for i in range(10000):
-        
-        '''
-        USERS GENERATE TASK
-        '''
-        graph.generate_task(i) 
+        graph.generate_task(i)
         graph.draw()
-        '''
-        USERS TRANSFER TASK to FOG
-        '''
         graph.transfer_task()
         graph.fog(i)
         graph.cloud(i)
 
-
-    data =  []
+    data = []
     for task in graph.completed:
         if task.start_time < 0:
             task.end_time -= task.start_time
@@ -190,15 +163,10 @@ def main():
             "priority": task.priority,
             "total_time": task.end_time - task.start_time
         })
+
     import json
-
-
     with open('data_p3.json', 'w') as outfile:
-        json.dump({"data"  : data , "fog_used" : graph.fog_used , "cloud_used" : graph.cloud_used}, outfile)
+        json.dump({"data": data, "fog_used": graph.fog_used, "cloud_used": graph.cloud_used}, outfile)
 
 if __name__ == "__main__":
     main()
-
-
-
-
--- SJR_Priority.py
+++ SJR_Priority.py
@@ -1,90 +1,82 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Function to set the random seed for reproducibility
 def seed():
     np.random.seed(0)
 
+# Function to calculate the Euclidean distance between two nodes
 def dist(node1, node2):
     return ((node1[0] - node2[0])**2 + (node1[1] - node2[1])**2)**0.5
 
-
-
-# class Task:
-#     def __init__(self, priority, time , start_time):
-#         self.priority = priority
-#         self.time = time
-#         self.task_time = time
-#         self.done = False
-#         self.start_time = start_time
-
+# Task class represents a computational task
 class Task:
-    def __init__(self, priority, time , start_time , deadline):
+    def __init__(self, priority, time, start_time, deadline):
         self.priority = priority
         self.exec_time = time
         self.done = False
         self.start_time = start_time
         self.deadline = deadline
-        self.cpu_cost = np.random.randint(0,10)
-        self.rom_cost = np.random.randint(0,10)
+        self.cpu_cost = np.random.randint(0, 10)
+        self.rom_cost = np.random.randint(0, 10)
 
     def __repr__(self):
         return f"Task({self.priority}, {self.time}, {self.done})"
 
+# Node class represents a computing node (FOG, USER, CLOUD)
 class Node:
-    def __init__(self, pos , type):
+    def __init__(self, pos, node_type):
         self.pos = pos
-        self.neighbors = []
-        self.type = type
-        self.tasks = []
+        self.neighbors = []  # Neighboring nodes
+        self.type = node_type
+        self.tasks = []  # Tasks assigned to the node
 
     def add_neighbor(self, node):
         self.neighbors.append(node)
 
     def draw(self):
+        # Plotting nodes based on type and tasks assigned
         if self.type == "FOG":
             if len(self.tasks) > 0:
-                plt.plot(self.pos[0], self.pos[1], 'ro' , markeredgecolor='r' , markersize=10)
+                plt.plot(self.pos[0], self.pos[1], 'ro', markeredgecolor='r', markersize=10)
             else:
-                plt.plot(self.pos[0], self.pos[1], 'ro' , markerfacecolor='none', markeredgecolor='r' , markersize=10)
+                plt.plot(self.pos[0], self.pos[1], 'ro', markerfacecolor='none', markeredgecolor='r', markersize=10)
         elif self.type == "CLOUD":
-            plt.plot(self.pos[0], self.pos[1], 'bo' , markerfacecolor='b', markeredgecolor='b' , markersize=10)
+            plt.plot(self.pos[0], self.pos[1], 'bo', markerfacecolor='b', markeredgecolor='b', markersize=10)
         else:
             if len(self.tasks) > 0:
-                plt.plot(self.pos[0], self.pos[1], 'go' , markeredgecolor='g' , markersize=10)
+                plt.plot(self.pos[0], self.pos[1], 'go', markeredgecolor='g', markersize=10)
             else:
-                plt.plot(self.pos[0], self.pos[1], 'go' , markerfacecolor='none', markeredgecolor='g' , markersize=10)
+                plt.plot(self.pos[0], self.pos[1], 'go', markerfacecolor='none', markeredgecolor='g', markersize=10)
+
+        # Drawing edges between neighboring nodes
         for n in self.neighbors:
             plt.plot([self.pos[0], n.pos[0]], [self.pos[1], n.pos[1]], 'black')
 
-
-
-
-
+# Graph class represents the entire computing graph
 class Graph:
     def __init__(self):
-        self.nodes = []
-        self.completed = []
-        self.cloud_used = 0
-        self.fog_used = 0
+        self.nodes = []  # List of nodes in the graph
+        self.completed = []  # Completed tasks
+        self.cloud_used = 0  # Total cloud processing time
+        self.fog_used = 0  # Total fog processing time
 
     def add_node(self, node):
         self.nodes.append(node)
 
-
     def connect(self):
         for i in range(len(self.nodes)):
-            if (self.nodes[i].type == "FOG"):
+            if self.nodes[i].type == "FOG":
                 self.nodes[i].add_neighbor(self.nodes[-1])
                 self.nodes[-1].add_neighbor(self.nodes[i])
 
-            if (self.nodes[i].type == "USER"):
-                min_dist = 100000
+            if self.nodes[i].type == "USER":
+                min_dist = float('inf')
                 min_node = None
                 for j in range(len(self.nodes)):
-                    if (self.nodes[j].type == "FOG"):
-                        
+                    if self.nodes[j].type == "FOG":
                         distance = dist(self.nodes[i].pos, self.nodes[j].pos)
-                        if (distance < min_dist):
+                        if distance < min_dist:
                             min_dist = distance
                             min_node = self.nodes[j]
                 self.nodes[i].add_neighbor(min_node)
@@ -93,13 +85,14 @@ class Graph:
         plt.clf()
         for node in self.nodes:
             node.draw()
-        plt.pause(0.0000000001)
+        plt.pause(1)
 
     def generate_task(self, time):
         for node in self.nodes:
-            if (node.type == "USER"):                                                            
+            if node.type == "USER":
+                # 20% probability of generating a task
                 if np.random.rand() < 0.2:
-                    node.tasks.append(Task(np.random.randint(1, 10), np.random.randint(1, 10) , time))
+                    node.tasks.append(Task(np.random.randint(1, 10), np.random.randint(1, 10), time, np.random.randint(5, 20)))
 
     def transfer_task(self):
         for node in self.nodes:
@@ -107,17 +100,16 @@ class Graph:
                 task = node.tasks.pop(0)
                 node.neighbors[0].tasks.append(task)
 
-    def fog(self , time):
+    def fog(self, time):
         for node in self.nodes:
             if node.type == "FOG" and len(node.tasks) > 0:
                 for task in node.tasks:
                     if task.priority < 5:
                         task.start_time -= 3
-                        
                         self.nodes[-1].tasks.append(task)
                         node.tasks.remove(task)
-                    
-                node.tasks.sort(key=lambda x: x.priority , reverse=True)
+
+                node.tasks.sort(key=lambda x: x.priority, reverse=True)
                 if len(node.tasks) > 0:
                     node.tasks[0].time -= 3
                     self.fog_used += 1.5
@@ -126,62 +118,47 @@ class Graph:
                         node.tasks[0].end_time = time
                         self.completed.append(node.tasks[0])
                         node.tasks.remove(node.tasks[0])
-                for i in range( 1, len(node.tasks)):
+                for i in range(1, len(node.tasks)):
                     self.nodes[-1].tasks.append(node.tasks.pop(-1))
-                    
 
-        
-    def cloud(self , time):
+    def cloud(self, time):
         for node in self.nodes:
             if node.type == "CLOUD" and len(node.tasks) > 0:
                 node.tasks.sort(key=lambda x: x.time)
-                # print(len(node.tasks))
                 i = 0
                 for task in node.tasks:
-                    self.cloud_used += task.time /0.95
+                    self.cloud_used += task.time / 0.95
                     task.done = True
-                    task.end_time = time 
+                    task.end_time = time
                     self.completed.append(task)
                     task.start_time -= 3
                     node.tasks.remove(task)
-                    i+= task.time // 4
+                    i += task.time // 4
 
-
-'''
-CONSTANTS
-'''
-
+# CONSTANTS
 FOG = 5
 USERS = 50
+
 def main():
     graph = Graph()
     for i in range(FOG):
-        graph.add_node(Node([np.random.randint(low = -100 , high = 100) , np.random.randint(low = -100 , high = 100)], "FOG"))
+        graph.add_node(Node([np.random.randint(low=-100, high=100), np.random.randint(low=-100, high=100)], "FOG"))
 
     for i in range(USERS):
-        graph.add_node(Node([np.random.randint(low = -300 , high = 300) , np.random.randint(low = -300 , high = 300)], "USER"))
-    
-    graph.add_node(Node([0,0] , "CLOUD"))
+        graph.add_node(Node([np.random.randint(low=-300, high=300), np.random.randint(low=-300, high=300)], "USER"))
 
+    graph.add_node(Node([0, 0], "CLOUD"))
 
     graph.connect()
     graph.generate_task(0)
     for i in range(10000):
-        
-        '''
-        USERS GENERATE TASK
-        '''
-        graph.generate_task(i) 
+        graph.generate_task(i)
         graph.draw()
-        '''
-        USERS TRANSFER TASK to FOG
-        '''
         graph.transfer_task()
         graph.fog(i)
         graph.cloud(i)
 
-
-    data =  []
+    data = []
     for task in graph.completed:
         if task.start_time < 0:
             task.end_time -= task.start_time
@@ -191,15 +168,10 @@ def main():
             "total_time": task.end_time - task.start_time,
             "task_time": task.task_time,
         })
+
     import json
-
-
     with open('data_sjf_priority3.json', 'w') as outfile:
-        json.dump({"data"  : data , "fog_used" : graph.fog_used , "cloud_used" : graph.cloud_used}, outfile)
+        json.dump({"data": data, "fog_used": graph.fog_used, "cloud_used": graph.cloud_used}, outfile)
 
 if __name__ == "__main__":
     main()
-
-
-
-
